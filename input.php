3451ddb022b593913645823a40ac9f9c13a7ef8e
AtqhPtr <putriistiatika13@gmail.com>
2024-10-21 15:16:03 +0700

    Koneksi PHP dengan Basisi Data

---
+++ input.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input</title>
+</head>
+<body>
+    <?php
+    $kecamatan = $_POST['kecamatan'];
+    $longitude = $_POST['longitude'];
+    $latitude = $_POST['latitude'];
+    $luas = $_POST['luas'];
+    $jumlah_penduduk = $_POST['jumlah_penduduk'];
+
+    $conn = new mysqli("localhost", "root","", "latihan");
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+    $sql = "INSERT INTO penduduk (kecamatan, longitude, latitude, luas, jumlah_penduduk)
+    VALUES ('$kecamatan', $longitude, $latitude, $luas, $jumlah_penduduk)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+
+    //header("Location: index.html");
+    //exit;
+    ?>
+</body>
+</html>
